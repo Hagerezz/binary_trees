ab0649a4ed4e69f050d5999197290bc5bf894dbe
hagerezz <queen.emilly12@gmail.com>
2023-09-01 04:02:56 +0300

    hi

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -8,11 +8,10 @@
 
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-    if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
-        return (NULL);
-
     binary_tree_t *grandparent = node->parent->parent;
 
+    if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
+        return (NULL);
     if (node->parent == grandparent->left)
         return (grandparent->right);
     else
