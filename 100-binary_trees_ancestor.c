511b90e47d9492c1ef62387c809dbb5245cb452d
Hagerezz <125587268+Hagerezz@users.noreply.github.com>
2023-09-02 01:00:07 +0300

    hi

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -1,17 +1,17 @@
 #include "binary_trees.h"
 
-/**
- * binary_tree_depthh - height of a binary tree
- * @tree: a binary tree
- * Return: the height of the binary tree
-*/
+// /**
+//  * binary_tree_depthh - height of a binary tree
+//  * @tree: a binary tree
+//  * Return: the height of the binary tree
+// */
 
-size_t binary_tree_depthh(const binary_tree_t *tree)
-{
-	if (tree == NULL || tree->parent == NULL)
-		return (0);
-	return (1 + binary_tree_depthh(tree->parent));
-}
+// size_t binary_tree_depthh(const binary_tree_t *tree)
+// {
+// 	if (tree == NULL || tree->parent == NULL)
+// 		return (0);
+// 	return (1 + binary_tree_depthh(tree->parent));
+// }
 
 /**
  * binary_trees_ancestor - returns the ancestor
@@ -31,8 +31,8 @@ binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tr
     if (binary_tree_depthh(first) == binary_tree_depthh(second))
         return (binary_trees_ancestor(first->parent, second->parent));
     if (binary_tree_depthh(first) > binary_tree_depthh(second))
-        return (binary_trees_ancestor(first, second->parent));
-    if (binary_tree_depthh(first) < binary_tree_depthh(second))
         return (binary_trees_ancestor(first->parent, second));
+    if (binary_tree_depthh(first) < binary_tree_depthh(second))
+        return (binary_trees_ancestor(first, second->parent));
     return (NULL);
 }
